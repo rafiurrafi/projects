ea77682e68eeb3a9dadbbf292abb20961dac0775
Rafiur Rahman <rrfoundary@gmail.com>
2022-01-13 05:23:52 +0600

    sigal - added join program section with slide

--- sigal/js/main.js
+++ sigal/js/main.js
@@ -1,3 +1,4 @@
+// isotop
 var $grid = $(".top-courses__content").isotope({
   // options
   filter: ".v",
@@ -6,4 +7,49 @@ var $grid = $(".top-courses__content").isotope({
 $(".top-courses__filter-btns").on("click", "button", function () {
   var filterValue = $(this).attr("data-filter");
   $grid.isotope({ filter: filterValue });
+});
+$(document).ready(function () {
+  //owl carousel
+
+  $("#upcoming-course").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 1,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false,
+      },
+    },
+  });
+  // join-card-container
+  $(".join-card-container ").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 1,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false,
+      },
+    },
+  });
 });

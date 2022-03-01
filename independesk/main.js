741530488a15b447a6c194d4489ec9a664b4bb93
Rafiur Rahman <rrfoundary@gmail.com>
2022-03-01 17:28:32 +0600

    independesk - added basic functionality of dropdown

--- independesk/main.js
+++ independesk/main.js
@@ -11,6 +11,47 @@ document.addEventListener("click", function (e) {
     document.querySelector(".dropbtn").classList.remove("active");
   }
 });
+// dropdown for sort
+function sortToggle(e) {
+  document.getElementById("sortDropdown").classList.toggle("show");
+  e.classList.toggle("active");
+}
+
+document.addEventListener("click", function (e) {
+  var container = document.querySelector(".sort-container");
+  if (!container.contains(e.target)) {
+    document.getElementById("sortDropdown").classList.remove("show");
+    document.querySelector(".sortDropdownBtn").classList.remove("active");
+  }
+});
+
+// dropdown for category
+function categoryToggle(e) {
+  document.getElementById("category").classList.toggle("show");
+  e.classList.toggle("active");
+}
+
+document.addEventListener("click", function (e) {
+  var container = document.querySelector("#category-container");
+  if (!container.contains(e.target)) {
+    document.getElementById("category").classList.remove("show");
+    document.querySelector(".categoryBtn").classList.remove("active");
+  }
+});
+
+// dropdown for Export
+function exportToggle(e) {
+  document.getElementById("export").classList.toggle("show");
+  e.classList.toggle("active");
+}
+
+document.addEventListener("click", function (e) {
+  var container = document.querySelector(".export-container");
+  if (!container.contains(e.target)) {
+    document.getElementById("export").classList.remove("show");
+    document.querySelector(".exportBtn").classList.remove("active");
+  }
+});
 
 // Close the dropdown if the user clicks outside of it
 // window.onclick = function (event) {

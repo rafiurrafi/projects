e0ba5c4a9ff141753b88afd5d66321d855c7a384
Rafiur Rahman <rrfoundary@gmail.com>
2022-04-04 17:52:05 +0600

    sigal - pagination for all tabs

--- sigal2.0/dashboard/js/paginate.js
+++ sigal2.0/dashboard/js/paginate.js
@@ -1,9 +1,21 @@
-const itemContainer = document.querySelector(".dash-tables");
+const itemContainer = document.querySelector(".dash-tables--online");
 const items = document.querySelectorAll("#online .dash-table");
 const paginationContainer = document.querySelector(".pagination--online");
 
+// inperson
+const itemPersonContainer = document.querySelector(".dash-tables--person");
+const itemsPerson = document.querySelectorAll("#inperson .dash-table");
+const paginationContainerPerson = document.querySelector(
+  ".pagination--inperson"
+);
+
+//hybrid
+const itemContainerHybrid = document.querySelector(".dash-tables--hybrid");
+const itemsHybrid = document.querySelectorAll("#hybrid .dash-table");
+const paginationContainerHybrid = document.querySelector(".pagination--hybrid");
+
 let current_page = 1;
-let rows = 2;
+let rows = 1;
 
 function displayList(items, wrapper, rowsPerPage, page) {
   wrapper.innerHTML = "";
@@ -16,15 +28,15 @@ function displayList(items, wrapper, rowsPerPage, page) {
     wrapper.appendChild(paginatedItems[i]);
   }
 }
-function setupPagination(items, wrapper, rowsPerPage) {
+function setupPagination(items, wrapper, rowsPerPage, itemContainer) {
   wrapper.innerHTML = "";
   let pageCount = Math.ceil(items.length / rowsPerPage);
   for (let i = 1; i < pageCount + 1; i++) {
-    let button = paginationButton(i, items);
+    let button = paginationButton(i, items, wrapper, itemContainer);
     wrapper.appendChild(button);
   }
 }
-function paginationButton(page, items) {
+function paginationButton(page, items, paginationWrapper, itemContainer) {
   let button = document.createElement("div");
   button.innerText = page;
   button.className =
@@ -35,7 +47,7 @@ function paginationButton(page, items) {
   button.addEventListener("click", function () {
     current_page = page;
     //remove active clss
-    const buttons = paginationContainer.querySelectorAll("div");
+    const buttons = paginationWrapper.querySelectorAll("div");
     buttons.forEach((button) => {
       button.classList.remove("active");
     });
@@ -46,4 +58,23 @@ function paginationButton(page, items) {
   return button;
 }
 displayList(items, itemContainer, rows, current_page);
-setupPagination(items, paginationContainer, rows);
+setupPagination(items, paginationContainer, rows, itemContainer);
+
+// inperson
+
+displayList(itemsPerson, itemPersonContainer, rows, current_page);
+setupPagination(
+  itemsPerson,
+  paginationContainerPerson,
+  rows,
+  itemPersonContainer
+);
+
+// hybrid
+displayList(itemsHybrid, itemContainerHybrid, rows, current_page);
+setupPagination(
+  itemsHybrid,
+  paginationContainerHybrid,
+  rows,
+  itemContainerHybrid
+);

f57f27c1471e535c91db41020871fa9eba1dae9c
Rafiur Rahman <rrfoundary@gmail.com>
2022-05-30 14:59:01 +0600

    one - added progress section and component

--- masterkey/one/js/tab.js
+++ masterkey/one/js/tab.js
@@ -1,5 +1,5 @@
 document.getElementById("defaultOpen").click();
-function openCity(evt, cityName) {
+function openCity(evt, tabName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -16,6 +16,6 @@ function openCity(evt, cityName) {
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
